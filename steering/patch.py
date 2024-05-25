d513652bb31514914f3d93a2682c7b8c42d80fe0
Sviatoslav <slava.chalnev@gmail.com>
2024-05-25 22:08:12 +1000

    Add 2d mixture evals

--- steering/patch.py
+++ steering/patch.py
@@ -13,7 +13,7 @@ import transformer_lens.utils as tutils
 from steering.eval_utils import evaluate_completions
 
 
-def patch_resid(resid, hook, steering, c=1, pos=0):
+def patch_resid(resid, hook, steering, c=1, pos=None):
     assert len(steering.shape) == 3 # [batch_size, sequence_length, d_model]
 
     if pos is None:
@@ -37,7 +37,7 @@ def get_loss(
         ds_name="NeelNanda/c4-code-20k",
         n_batches=50,
         batch_size=8,
-        insertion_pos=0,
+        insertion_pos=None,
     ):
     if scales is None:
         scales = [1]
@@ -113,7 +113,7 @@ def get_scores_and_losses(
     criterion: str,
     scales: list[float],
     n_samples=10,
-    insertion_pos=0,
+    insertion_pos=None,
     explanations=True,
     word_list=None,
     top_k=50,
@@ -158,3 +158,103 @@ def get_scores_and_losses(
         return mean_scores, all_scores, sae_anger_losses, word_probabilities
     else:
         return mean_scores, all_scores, sae_anger_losses
+
+
+@torch.no_grad()
+def grid_losses(
+        model: HookedTransformer,
+        hook_point: str,
+        vector_grid: torch.Tensor,
+        ds_name="NeelNanda/c4-code-20k",
+        n_batches=50,
+        batch_size=8,
+        insertion_pos=None,
+    ):
+    """
+    Used in scores_2d
+    """
+    loss_grid = torch.zeros((vector_grid.shape[0], vector_grid.shape[1]), device=vector_grid.device)
+
+    print(f'loading dataset: {ds_name}')
+    data = load_dataset(ds_name, split="train")
+    tokenized_data = tutils.tokenize_and_concatenate(data, model.tokenizer, max_length=128)
+    tokenized_data = tokenized_data.shuffle(42)
+    loader = DataLoader(tokenized_data, batch_size=batch_size)
+    print('dataset loaded')
+
+    for i in tqdm(range(vector_grid.shape[0])):
+        for j in range(vector_grid.shape[1]):
+            steering = vector_grid[i, j]
+            steering = steering[None, None, :]
+            total_loss = 0
+            for batch_idx, batch in enumerate(loader):
+                with model.hooks(fwd_hooks=[(hook_point, partial(patch_resid,
+                                                                steering=steering,
+                                                                c=1,
+                                                                pos=insertion_pos,
+                                                                ))]):
+                    loss = model(batch["tokens"], return_type="loss", prepend_bos=False)
+                    total_loss += loss.item()
+                if batch_idx >= n_batches-1:
+                    break
+            loss_grid[i, j] = total_loss / n_batches
+    return loss_grid
+
+
+@torch.no_grad()
+def scores_2d(
+    model: HookedTransformer,
+    hook_point: str,
+    steering_vectors: list[torch.Tensor],
+    prompt: str,
+    criterions: list[str],
+    scales: list[float],
+    n_samples=10,
+    insertion_pos=None,
+    top_k=50,
+    ):
+    assert len(steering_vectors) == len(criterions) == 2
+
+    v1, v2 = steering_vectors
+    device = v1.device
+
+    vector_grid = torch.zeros((len(scales), len(scales), v1.shape[-1]), device=device)
+    for i, s1 in enumerate(scales):
+        for j, s2 in enumerate(scales):
+            vector_grid[i, j] = v1 * s1 + v2 * s2
+
+    loss_grid = grid_losses(model, hook_point, vector_grid, insertion_pos=insertion_pos)
+    print("loss grid")
+    print(loss_grid)
+
+    score_grid_1 = torch.zeros_like(loss_grid, device=device)
+    score_grid_2 = torch.zeros_like(loss_grid, device=device)
+
+    for i in range(len(scales)):
+        for j in range(len(scales)):
+            print(f'evaluating ({i}, {j})')
+            steering_vector = vector_grid[i, j]
+            gen_texts = generate(model,
+                                hook_point,
+                                prompt=prompt,
+                                steering_vector=steering_vector[None, None, :],
+                                scale=1,
+                                n_samples=n_samples,
+                                insertion_pos=insertion_pos,
+                                top_k=top_k,
+                                )
+
+            eval_1 = evaluate_completions(gen_texts, criterion=criterions[0], prompt=prompt, verbose=False)
+            eval_2 = evaluate_completions(gen_texts, criterion=criterions[1], prompt=prompt, verbose=False)
+            print(gen_texts)
+
+            scores_1 = [e['score'] for e in eval_1]
+            scores_2 = [e['score'] for e in eval_2]
+            mean_1 = sum(scores_1) / len(scores_1)
+            mean_2 = sum(scores_2) / len(scores_2)
+            score_grid_1[i, j] = mean_1
+            score_grid_2[i, j] = mean_2
+            print(criterions[0], mean_1)
+            print(criterions[1], mean_2)
+        
+    return score_grid_1.cpu(), score_grid_2.cpu(), loss_grid.cpu()
